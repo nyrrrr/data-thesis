baf5bba22740f4b9901021b79fbe58027b9579c1
nyrrrr <info@nyrrrr.com>
2016-12-20 21:43:47 +0100

    added missing features like skewness and variance

--- R/extract-features-from-backlogger-data.R
+++ R/extract-features-from-backlogger-data.R
@@ -23,7 +23,6 @@ for (i in 1:nrow(rawdata)) {
     tmpRawBetaWindow <- rawdata$beta[rawdata$id >= i - wJumper - 1 & rawdata$id < i + wJumper];
     tmpRawGammaWindow <- rawdata$gamma[rawdata$id >= i - wJumper - 1 & rawdata$id < i + wJumper];
     
-    
     # Min (x) Accelerometer
     fsd$XminA[wIndex] <- min(tmpRawXWindow);
     # Min (y) Accelerometer
@@ -60,10 +59,54 @@ for (i in 1:nrow(rawdata)) {
     fsd$YmeanG[wIndex] <- mean(tmpRawBetaWindow);
     # Mean (z) Gyroscope
     fsd$ZmeanG[wIndex] <- mean(tmpRawGammaWindow);
-    
-    
-    
-    
+    # Median (x) Accelerometer
+    fsd$XmedianA[wIndex] <- median(tmpRawXWindow);
+    # Median (y) Accelerometer
+    fsd$YmedianA[wIndex] <- median(tmpRawYWindow);
+    # Median (z) Accelerometer
+    fsd$ZmedianA[wIndex] <- median(tmpRawZWindow);
+    # Median (x) Gyroscope
+    fsd$XmedianG[wIndex] <- median(tmpRawAlphaWindow);
+    # Median (y) Gyroscope
+    fsd$YmedianG[wIndex] <- median(tmpRawBetaWindow);
+    # Median (z) Gyroscope
+    fsd$ZmedianG[wIndex] <- median(tmpRawGammaWindow);
+    # Standard Deviation (x) Accelerometer
+    fsd$XsdA[wIndex] <- sd(tmpRawXWindow);
+    # Standard Deviation (y) Accelerometer
+    fsd$YsdA[wIndex] <- sd(tmpRawYWindow);
+    # Standard Deviation (z) Accelerometer
+    fsd$ZsdA[wIndex] <- sd(tmpRawZWindow);
+    # Standard Deviation (x) Gyroscope
+    fsd$XsdG[wIndex] <- sd(tmpRawAlphaWindow);
+    # Standard Deviation (y) Gyroscope
+    fsd$YsdG[wIndex] <- sd(tmpRawBetaWindow);
+    # Standard Deviation (z) Gyroscope
+    fsd$ZsdG[wIndex] <- sd(tmpRawGammaWindow);
+    # Variance (x) Accelerometer
+    fsd$XvarA[wIndex] <- var(tmpRawXWindow);
+    # Variance (y) Accelerometer
+    fsd$YvarA[wIndex] <- var(tmpRawYWindow);
+    # Variance (z) Accelerometer
+    fsd$ZvarA[wIndex] <- var(tmpRawZWindow);
+    # Variance (x) Gyroscope
+    fsd$XvarG[wIndex] <- var(tmpRawAlphaWindow);
+    # Variance (y) Gyroscope
+    fsd$YvarG[wIndex] <- var(tmpRawBetaWindow);
+    # Variance (z) Gyroscope
+    fsd$ZvarG[wIndex] <- var(tmpRawGammaWindow);
+    # Skewness (x) Accelerometer
+    fsd$XskewA[wIndex] <- 3 * (fsd$XmeanA[wIndex] - fsd$XmedianA[wIndex])/fsd$XsdA[wIndex]
+    # Skewness (y) Accelerometer
+    fsd$YskewA[wIndex] <- 3 * (fsd$YmeanA[wIndex] - fsd$YmedianA[wIndex])/fsd$YsdA[wIndex]
+    # Skewness (z) Accelerometer
+    fsd$ZskewA[wIndex] <- 3 * (fsd$ZmeanA[wIndex] - fsd$ZmedianA[wIndex])/fsd$ZsdA[wIndex]
+    # Skewness (x) Gyroscope
+    fsd$XskewG[wIndex] <- 3 * (fsd$XmeanG[wIndex] - fsd$XmedianG[wIndex])/fsd$XsdG[wIndex]
+    # Skewness (y) Gyroscope
+    fsd$YskewG[wIndex] <- 3 * (fsd$YmeanG[wIndex] - fsd$YmedianG[wIndex])/fsd$YsdG[wIndex]
+    # Skewness (z) Gyroscope
+    fsd$ZskewG[wIndex] <- 3 * (fsd$ZmeanG[wIndex] - fsd$ZmedianG[wIndex])/fsd$ZsdG[wIndex]
     # RMS(x) Accelerometer
     fsd$XrmsA[wIndex] <- sqrt(sum((tmpRawXWindow) ^ 2) / wSize)
     # RMS(y) Accelerometer
@@ -76,10 +119,8 @@ for (i in 1:nrow(rawdata)) {
     fsd$YrmsG[wIndex] <- sqrt(sum((tmpRawBetaWindow) ^ 2) / wSize)
     # RMS(z) Gyroscope
     fsd$ZrmsG[wIndex] <- sqrt(sum((tmpRawGammaWindow) ^ 2) / wSize)
-    
     # Root Mean Square of the magnitude of the Accelerometer vector
     fsd$MagnRmsA[wIndex] <- sqrt((sum((tmpRawXWindow) ^ 2) + sum((tmpRawYWindow) ^ 2) + sum((tmpRawZWindow) ^ 2)) / wSize)
-    
     # Root Mean Square of the magnitude of the Gyroscope vector
     fsd$MagnRmsG[wIndex] <- sqrt((sum((tmpRawAlphaWindow) ^ 2) + sum((tmpRawBetaWindow) ^ 2) + sum((tmpRawGammaWindow) ^ 2)) / wSize) 
     # time of window
