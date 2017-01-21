29f7c503cff1d9d3a2c48d7d17d74b382f524660
nyrrrr <info@nyrrrr.com>
2017-01-21 23:14:11 +0100

    fixed lin & cubic interp

--- R/extract-features.R
+++ R/extract-features.R
@@ -3,10 +3,10 @@ options(digits=20)
 
 source("extract-features-class.R")
 
-flist <- c(17011417)#, 17011205, 17011020, 16122802)
+flist <- c(17011020)#, 16122802)
 
 for ( i in flist ) {
-  feature.extraction(boolTraining = TRUE,stringFileTimestamp = i,boolLabeled = TRUE)
-  #if(i != 17011906) feature.extraction(boolTraining = FALSE,stringFileTimestamp = i,boolLabeled = TRUE)
+  # feature.extraction(boolTraining = TRUE,stringFileTimestamp = i,boolLabeled = TRUE)
+  fKeyTrain <- feature.extraction(boolTraining = FALSE,stringFileTimestamp = i,boolLabeled = TRUE)
 }
 
